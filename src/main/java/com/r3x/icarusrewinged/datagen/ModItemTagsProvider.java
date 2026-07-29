7c83787bfc9eb357f5d1a8d152b77c8cb9279f40
r3ynex <r3ynex.error@gmail.com>
2026-07-29 13:10:23 +0300

    Added Melts Tag

--- src/main/java/com/r3x/icarusrewinged/datagen/ModItemTagsProvider.java
+++ src/main/java/com/r3x/icarusrewinged/datagen/ModItemTagsProvider.java
@@ -23,15 +23,22 @@ public class ModItemTagsProvider extends ItemTagsProvider {
     protected void addTags(HolderLookup.Provider provider) {
 
         TagKey<Item> ICARUS_WINGS = ItemTags.create(ResourceLocation.fromNamespaceAndPath("icarus", "wings"));
-
         var icarusBuilder = this.tag(ICARUS_WINGS);
 
         IcarusReItems.ITEMS.getEntries().forEach(registryObject -> {
             Item item = registryObject.get();
-
             if (item != IcarusReItems.MOD_LOGO.get()) {
-
                 icarusBuilder.add(item);
+            }
+        });
+
+        TagKey<Item> ICARUS_MELTS = ItemTags.create(ResourceLocation.fromNamespaceAndPath("icarus", "melts"));
+        var meltsBuilder = this.tag(ICARUS_MELTS);
+
+        IcarusReItems.ITEMS.getEntries().forEach(registryObject -> {
+            Item item = registryObject.get();
+            if (item != IcarusReItems.MOD_LOGO.get()) {
+                meltsBuilder.add(item);
             }
         });
     }
